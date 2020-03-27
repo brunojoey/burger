66fe85494a4925f5819e82da35d55d0ff17d34ee
Joseph Bruno <brunojoey12@gmail.com>
2020-03-26 22:49:29 -0500

    Set up the folders for the entire application.

--- config/orm.js
+++ config/orm.js
@@ -3,13 +3,16 @@ const connection = require("./connection");
 var orm = {
     selectAll: function(tableInput, colToSearch, valOfCol) {
         connection.query("SELECT * FROM ")
-    };
+    },
 
     insertOne: function(tableInput, colToSearch, valOfCol) {
+        connection.query("INSERT INTO burgers (burger_name, devoured)")
 
-    };
+    },
 
     updateOne: function(tableInput, colToSearch, valOfCol) {
 
-    };
+    }
 }
+
+module.exports = orm;

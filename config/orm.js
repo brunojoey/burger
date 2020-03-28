c1acca5555a8a7ccbee7fba4254e63965281dc2f
Joseph Bruno <brunojoey12@gmail.com>
2020-03-27 19:41:16 -0500

    Setup the orm, controller, and model file for the project.

--- config/orm.js
+++ config/orm.js
@@ -1,18 +1,51 @@
 const connection = require("./connection");
 
+function objectSQL(ob) {
+    var arr = [];
+  
+    for (var key in ob) {
+      var value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        };
+
+        arr.push(key + "=" + value);
+      };
+    };
+      return arr.toString();
+  };
+  
+
 var orm = {
-    selectAll: function(tableInput, colToSearch, valOfCol) {
-        connection.query("SELECT * FROM ")
+    selectAll: function(cb) {
+        connection.query("SELECT * FROM ", function(err,result) {
+            if(err) throw err;
+            cb(result);
+        });
     },
 
-    insertOne: function(tableInput, colToSearch, valOfCol) {
-        connection.query("INSERT INTO burgers (burger_name, devoured)")
+    insertOne: function(cb) {
+        connection.query("INSERT INTO burgers (burger_name, devoured) VALUES ?,?", function(err, result) {
+            if(err) throw err;
+            cb(result);
+        });
 
     },
 
-    updateOne: function(tableInput, colToSearch, valOfCol) {
-
+    updateOne: function(objColVals, condition, cb) {
+        var query = "UPDATE burgers (burger_name, devoured)"
+        query += " SET ";
+        query += objectSQL(objColVals);
+        query += " WHERE ";
+        query += condition;
+    
+        connection.query(query, function(err, result) {
+            if(err) throw err;
+            cb(result);
+        });
     }
-}
+};
 
 module.exports = orm;

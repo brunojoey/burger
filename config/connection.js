2e4133cb695ed5bb58248dc75228a9b6a0ee6f1c
Joey Bruno <brunojoey12@gmail.com>
2020-04-05 13:19:52 -0500

    Added const to connection.

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 // mysql://nld9y9seikt0wd1i:e41qcuuow33pkqsd@zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lwz2ko4048mv7bu5
 const mysql = require("mysql");
+let connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection.mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-  const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
     user: "nld9y9seikt0wd1i",
     password: "e41qcuuow33pkqsd",
     database: "lwz2ko4048mv7bu5"
   });
-// }
+}
 
 
 connection.connect(function(err) {

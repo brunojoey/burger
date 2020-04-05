80c5767b595a452c2c436c8b15521a092e79d65e
Joey Bruno <brunojoey12@gmail.com>
2020-04-05 13:17:33 -0500

    Commented out JAWSDB in connection.

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 // mysql://nld9y9seikt0wd1i:e41qcuuow33pkqsd@zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lwz2ko4048mv7bu5
 const mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection.mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWSDB_URL) {
+//   connection.mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
   connection = mysql.createConnection({
     host: "zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     password: "e41qcuuow33pkqsd",
     database: "lwz2ko4048mv7bu5"
   });
-}
+// }
 
 
 connection.connect(function(err) {
